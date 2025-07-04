857e2acc367b2fc5ab9c325ade76e0ec386e1011
Reileen00 <100047799+Reileen00@users.noreply.github.com>
2025-07-04 17:36:42 +0530

    Update receiver.py

--- Screen sharing/receiver.py
+++ Screen sharing/receiver.py
@@ -3,10 +3,10 @@ import threading
 
 receiver=StreamingServer('192.168.0.17',9999)
 
-t=threading.Thread(target=receiver.start_server)
+t=threading.Thread(target=receiver.start_stream())
 t.start()
 
 while input("") != 'STOP':
   continue
 
-receiver.stop_server()
+sender.stop_server()
